e723564f366be0a51b344b6e082c7d6de0062d56
App Academy Student <appacademy@Apps-Mac-mini-5.local>
2015-04-09 18:25:12 -0400

    Resting on bonus

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :user_name, presence: true
+
   has_many(
     :authored_polls,
     class_name: "Poll",
@@ -13,4 +14,41 @@ class User < ActiveRecord::Base
     foreign_key: :user_id,
     primary_key: :id
   )
+
+  def complete_polls
+    temp = Poll
+      .select("polls.*, COUNT(DISTINCT questions.id) as count" )
+      .joins("Left Join questions ON questions.poll_id = polls.id")
+      .joins("Left Join responses ON questions.id = responses.question_id")
+      .group("polls.id")
+
+    res = Response
+        .select("questions.poll_id, COUNT(responses.question_id) as count1")
+        .joins("LEFT JOIN questions ON responses.question_id = questions.id")
+        .where("responses.user_id = ?", self.id)
+        .group("questions.poll_id")
+
+
+    #   Select polls.*, count(distinct questions.id)
+    #   from
+    #   polls
+    #   left join
+    #   questions on questions.poll_id = polls.id
+    #   left join responses On questions.id = responses.question_id
+    #   Group By polls.id
+    #
+    #
+    # Select
+    # questions.poll_id, COUNT(responses.question_id) as NUM
+    # From
+    #   responses
+    # Join
+    #   questions ON responses.question_id = questions.id
+    # Where
+    #   responses.user_id = 58
+    # Group By
+    #   questions.poll_id
+
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,13 +18,18 @@ a.save!
 
 Poll.delete_all
 p = Poll.new(author_id: i.id, title: "What?!")
-
+p1 = Poll.new(author_id: c.id, title: "HUH?!")
 p.save!
+p1.save!
 
 Question.delete_all
 q = Question.new(poll_id: p.id, text: "WHAT AM I DOING?!")
-
+q1 = Question.new(poll_id: p.id, text: "WHERE AM I?!")
 q.save!
+q1.save!
+
+q2 = Question.new(poll_id: p1.id, text: "DRINK?!")
+q2.save!
 
 AnswerChoice.delete_all
 a1 = AnswerChoice.new(question_id: q.id, text: "I DONT KNOW")
@@ -33,11 +38,26 @@ a2 = AnswerChoice.new(question_id: q.id, text: "BEING A BOSS")
 a1.save!
 a2.save!
 
+a3 = AnswerChoice.new(question_id: q1.id, text: "Outside")
+a4 = AnswerChoice.new(question_id: q1.id, text: "Inside")
+
+a3.save!
+a4.save!
+
+a5 = AnswerChoice.new(question_id: q2.id, text: "YES")
+a5.save!
+
 Response.delete_all
 r1 = Response.create!(user_id: b.id, question_id: q.id, answer_choice_id: a1.id)
 r2 = Response.create!(user_id: c.id, question_id: q.id, answer_choice_id: a2.id)
 r3 = Response.create!(user_id: a.id, question_id: q.id, answer_choice_id: a2.id)
+r4 = Response.create!(user_id: b.id, question_id: q1.id, answer_choice_id: a3.id)
+r5 = Response.create!(user_id: b.id, question_id: q2.id, answer_choice_id: a5.id)
+r6 = Response.create!(user_id: a.id, question_id: q2.id, answer_choice_id: a5.id)
 
 r1.save!
 r2.save!
 r3.save!
+r4.save!
+r5.save!
+r6.save!
